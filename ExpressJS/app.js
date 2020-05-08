37c144fe522b730e3e7e6f2d9d07d721d8438e9d
M. Mare <michael.mare@soprasteria.com>
2020-05-08 16:01:36 +0200

    added PUG and Handlebars

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,9 +1,29 @@
 const http = require("http");
 
-const express = require('express')
+const path = require("path");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const expresHbs = require('express-handlebars')
 
 const app = express();
 
-const server = http.createServer(app);
+app.engine('hbs', expresHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', 'views')
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+// ajout du fichier css en donnant accès au dossier 'public'
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
+app.listen(3000);
