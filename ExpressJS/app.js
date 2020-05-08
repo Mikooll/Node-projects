da6036424aa4c57479e09fe8615ec734ec60923a
M. Mare <michael.mare@soprasteria.com>
2020-05-08 16:47:38 +0200

    added EJS template

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -7,13 +7,11 @@ const shopRoutes = require("./routes/shop");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expresHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('hbs', expresHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
-app.set('views', 'views')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // ajout du fichier css en donnant accès au dossier 'public'
@@ -23,7 +21,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
